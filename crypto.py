3661cdbbbdadb1c6457c633c3b3e3fb11605dd3a
Saumay Rustagi <saumay03pro@gmail.com>
2023-01-19 16:05:38 +0530

    Add images

--- crypto.py
+++ crypto.py
@@ -2,7 +2,7 @@ import hashlib
 
 def hash_pass(img_arr):
 	def hash_file(filename):
-		h = hashlib.sha256()
+		h = hashlib.md5()
 
 		with open(filename, 'rb') as file:
 			chunk = 0
@@ -13,12 +13,17 @@ def hash_pass(img_arr):
 		return h.hexdigest()
 
 	running_hash=""
+
 	for img in img_arr:
 		running_hash += hash_file(img)
 		print(running_hash)
-	running_hash = hashlib.sha256()
-	return running_hash.hexdigest()
+	
+	final_hash = hashlib.sha256(running_hash.encode('UTF-8'))
+	return final_hash.hexdigest()
 
-arr = ["a.jpg","b.jpg","c.jpg"]
-print(hash_pass(arr))
-
+arr = []
+for i in range(1,33):
+	st = "random-images/img"+str(i)+".jpg"
+	arr.append(st)
+print(arr)
+print("\n"+hash_pass(arr))
